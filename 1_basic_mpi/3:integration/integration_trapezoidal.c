477b4b11cc582284e9f3d2e96c1d696db1858b1d
Chan Park <iamparkchan@users.noreply.github.com>
2019-07-16 06:51:46 +0000

    add integration

---
+++ 1_basic_mpi/3:integration/integration_trapezoidal.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <math.h>
+#include <mpi.h>
+
+#define N 100000000
+#define A 0.0
+#define B 1.0
+
+double f(double x)
+{
+    return x*x;
+}
+
+double solution(double a, double b)
+{
+	return b*b*b/3.0 - a*a*a/3.0;
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+	double my_sum = 0.0, sum = 0.0, h = (B - A) / N;
+	double start, end;
+	int rank, size;
+    int unit, i_s, i_e;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	start = MPI_Wtime();
+	unit = (N + size - 1) / size;
+    if (rank == 0) {
+        i_s = 1;
+        i_e = unit;
+        my_sum = f(A)/2.0;
+    } else if (rank == size - 1) {
+        i_s = rank*unit;
+        i_e = N;
+        my_sum = f(B)/2.0;
+    } else {
+        i_s = rank*unit;
+        i_e = (rank + 1)*unit;
+        my_sum = 0.0;
+    }
+	for (i = i_s; i < i_e; i++) {
+		my_sum += f(A + i*h);
+	}
+	if (0 != rank) {
+		MPI_Send(&my_sum, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+	} else {
+		sum += my_sum;
+		for (i = 1; i < size; i++) { 
+			MPI_Recv(&my_sum, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			sum += my_sum;
+		}
+		sum *= h;
+	}
+	end = MPI_Wtime();
+
+	if (0 == rank) {
+		printf("result=%e\n", sum);
+		printf("error=%e\n", fabs(sum - solution(A, B)));
+		printf("elapsed time=%f\n", end - start);
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
